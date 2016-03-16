09175fb9494aea4bfa2f658729b35b6c9387bcdd
Shohei Aoki <shoaok@gmail.com>
2016-03-16 09:52:16 +0000

    finished html template

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
-
 #!/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import os
 import tornado.ioloop
 import tornado.web
+import tornado.autoreload
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
 application = tornado.web.Application([
-    (r"/", MainHandler)
+    (r"/", MainHandler),
     ],
+    debug=True,
     template_path=os.path.join(os.getcwd(),  "templates"),
     static_path=os.path.join(os.getcwd(),  "static"),
-)
+    )
 
 if __name__ == "__main__":
     application.listen(8888)
